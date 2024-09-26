8bc4be301151aa555873255c250a955c76dc2bd0
mahesh.sabnis <mahesh.sabnis@imriel.com>
2024-09-26 11:51:30 +0530

    Add project files.

---
+++ CS_AcceptData/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using CS_Models;
+using System.Text.Json;
+
+Console.WriteLine("Enter Order Data Here");
+
+try
+{
+
+    Order order = AcceptOrderData();
+    AddOrderToQueue(order);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error Occurred : {ex.Message}");
+}
+
+
+Console.ReadLine();
+
+static Order AcceptOrderData()
+{
+    Order order = new Order();
+    Console.WriteLine("Enter Order Id");
+    order.OrderId = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter Order Name");
+    order.OrderName = Console.ReadLine();
+    Console.WriteLine("Enter Quantity");
+    order.Quantity = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter Advance");
+    order.Advance = Convert.ToInt32(Console.ReadLine());
+
+    return order;
+}
+
+
+static void AddOrderToQueue(Order order)
+{
+    try
+    {
+        // Replace with your connection string
+        string connectionString = "";
+        string queueName = "orders";
+
+        // Define an access policy
+        QueueSignedIdentifier accessPolicy = new QueueSignedIdentifier
+        {
+            Id = "mypolicy",
+            AccessPolicy = new QueueAccessPolicy
+            {
+                // Set the expiry time to 5 Minutes from now
+                ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(5),
+                // Set the start time to now
+                StartsOn = DateTimeOffset.UtcNow,
+                // Set the permissions to allow adding messages
+                Permissions = "rau"
+            }
+        };
+
+        // Create a QueueClient
+        QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+        // Create the queue if it doesn't already exist
+        queueClient.CreateIfNotExists();
+
+        // Set the access policy on the queue
+        queueClient.SetAccessPolicy(new QueueSignedIdentifier[] { accessPolicy });
+
+
+
+        if (queueClient.Exists())
+        {
+            // Serialize the order object to JSON
+            string orderJson = JsonSerializer.Serialize(order);
+
+            // Add the message to the queue
+            queueClient.SendMessage(orderJson);
+
+            Console.WriteLine("Order added to the queue successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Failed to create or connect to the queue.");
+        }
+    }
+    catch (Exception ex)
+    {
+        throw ex;
+    }
+}
---
+++ CS_Models/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string? OrderName { get; set; }
+        public int Quantity { get; set; }
+        public int TotalPrice { get; set; }
+
+        public int Advance { get; set; }
+
+    }
+
+    public class Items
+    {
+        public string? ItemName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+
+    public class ItemsDb : List<Items>
+    {
+        public ItemsDb()
+        {
+            Add(new Items() { ItemName = "Laptop", UnitPrice = 120000 });
+            Add(new Items() { ItemName = "Dektop", UnitPrice = 60000 });
+            Add(new Items() { ItemName = "RAM", UnitPrice = 8000 });
+            Add(new Items() { ItemName = "Monitor", UnitPrice = 6450 });
+            Add(new Items() { ItemName = "SSD", UnitPrice = 20000 });
+            Add(new Items() { ItemName = "HDD", UnitPrice = 8610 });
+        }
+    }
+}
